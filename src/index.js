607e65c29cbe4ff031e93bd7dbef26d987663764
Lucas Maciel <lucas.maciel@wisereducacao.com>
2020-11-05 09:04:23 -0300

    feat: commit initial

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const util = require("./util");
+
+console.log(util.soma(1, 5));
---
+++ src/util.js
@@ -0,0 +1,12 @@
+function soma(a, b) {
+  return a + b;
+}
+
+function mod(a, b) {
+  return a % b;
+}
+
+module.exports = {
+  soma,
+  mod,
+};
